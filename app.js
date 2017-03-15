a002f076eaf49e4370d051a625b5822b23c2cd1e
Riza Fahmi <rizafahmi@gmail.com>
2017-03-15 20:28:08 +0700

    Finish refactor to module profile

--- app.js
+++ app.js
@@ -1,34 +1,5 @@
-// TODO: [x] Connect to GitHub API
-let https = require('https')
+let profile = require('./profile.js')
 
-const options = {
-  hostname: 'api.github.com',
-  port: 443,
-  path: '/users/yofri',
-  method: 'GET',
-  headers: {
-    'user-agent': 'nodejs'
-  }
-}
-
-// TODO: [x] Read the data
-let request = https.request(options, (response) => {
-  let body = ''
-  response.on('data', (data) => {
-    body = body + data
-  })
-  response.on('end', () => {
-    // TODO: [x] Parse the data
-    // Convert String to JSON (JavaScript object)
-    let profile = JSON.parse(body)
-    // console.log(profile.login + ' owns ' + profile.public_repos + ' repo(s) ')
-    // TODO: [x] Print the data out
-    console.log(`${profile.login} owns ${profile.public_repos} repo(s) and has ${profile.followers} followers(s).`)
-  })
-})
-
-request.end()
-
-request.on('error', (e) => {
-  console.error(e)
-})
+profile.get('rizafahmi')
+profile.get('yofri')
+profile.get('ronishak')
